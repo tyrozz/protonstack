bcdb11c1984db727a736f269b65f9804764068ad
ozan yildirim <ozanyildirim1000@gmail.com>
2023-07-27 23:03:57 +0100

    pricing page updated

--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -1,8 +1,26 @@
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
 import { getActiveProductsWithPrices } from '@/lib/subscriptions'
-
+import { siteConfig } from '@/config/site'
 import { Pricing } from '@/components/pricing'
+import { SiteHeader } from '@/components/site-header'
+import { cn } from '@/lib/utils'
 
 export default async function PricingPage() {
     const productsWithPrices = await getActiveProductsWithPrices()
-    return <Pricing products={productsWithPrices} />
+    return (
+        <>
+            <SiteHeader />
+            <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+                <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+                    <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">Pricing </h1>
+                    <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu sagittis lorem. Nulla
+                        nec mi molestie, suscipit libero ac, semper dolor.{' '}
+                    </p>
+                    <Pricing products={productsWithPrices} />
+                </div>
+            </section>
+        </>
+    )
 }
--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -47,73 +47,58 @@ export function Pricing({ products }: PricingListProps) {
     }
 
     return (
-        <div className="bg-white dark:bg-gray-900">
-            <div className="container px-6 py-8 mx-auto">
-                <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
-                    Pricing Plan
-                </h1>
+        <div className="grid grid-cols-1 gap-8 mt-6 xl:mt-12 xl:gap-12 md:grid-cols-2 lg:grid-cols-3">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Free</CardTitle>
+                    <CardDescription>Free Subscription for a month</CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4"></CardContent>
+                <CardFooter>
+                    <CardFooter>{!userId && <Button className="w-full">Start now</Button>}</CardFooter>
+                </CardFooter>
+            </Card>
 
-                <p className="max-w-2xl mx-auto mt-4 text-center text-gray-500 xl:mt-6 dark:text-gray-300">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias quas magni libero consequuntur
-                    voluptatum velit amet id repudiandae ea, deleniti laborum in neque eveniet.
-                </p>
-                <div className="grid grid-cols-1 gap-8 mt-6 xl:mt-12 xl:gap-12 md:grid-cols-2 lg:grid-cols-3">
-                    <Card className="w-[350px]">
+            {products &&
+                products.map((product) => (
+                    <Card key={product.id} className={cn('w-[380px]')}>
                         <CardHeader>
-                            <CardTitle>Free</CardTitle>
-                            <CardDescription>Free Subscription for a month</CardDescription>
+                            <CardTitle>{product.name} Plan</CardTitle>
+                            <CardDescription>{product.description}</CardDescription>
                         </CardHeader>
-                        <CardContent className="grid gap-4"></CardContent>
-                        <CardFooter>
-                            <CardFooter>{!userId && <Button className="w-full">Start now</Button>}</CardFooter>
-                        </CardFooter>
-                    </Card>
-
-                    {products &&
-                        products.map((product) => (
-                            <Card key={product.id} className={cn('w-[380px]')}>
-                                <CardHeader>
-                                    <CardTitle>{product.name} Plan</CardTitle>
-                                    <CardDescription>{product.description}</CardDescription>
-                                </CardHeader>
-                                <CardContent className="grid gap-4">
-                                    <div>
-                                        {product.prices.map((price, index) => (
-                                            <div
-                                                key={index}
-                                                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                                            >
-                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                                <div className="space-y-1">
-                                                    <p className="text-sm font-medium leading-none">
-                                                        {price.currency} {(price.unit_amount as number) / 100}
-                                                    </p>
-                                                    <p className="text-sm text-muted-foreground">
-                                                        per {price.recurring?.interval}
-                                                    </p>
-                                                    {!userId && (
-                                                        <Button className="w-full">
-                                                            <Link href="/sign-in">Start Now</Link>
-                                                        </Button>
-                                                    )}
-                                                    {userId && (
-                                                        <Button
-                                                            onClick={() => handleCheckout(price)}
-                                                            className="w-full"
-                                                        >
-                                                            Checkout
-                                                        </Button>
-                                                    )}
-                                                </div>
-                                            </div>
-                                        ))}
+                        <CardContent className="grid gap-4">
+                            <div>
+                                {product.prices.map((price, index) => (
+                                    <div
+                                        key={index}
+                                        className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                                    >
+                                        <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                                        <div className="space-y-1">
+                                            <p className="text-sm font-medium leading-none">
+                                                {price.currency} {(price.unit_amount as number) / 100}
+                                            </p>
+                                            <p className="text-sm text-muted-foreground">
+                                                per {price.recurring?.interval}
+                                            </p>
+                                            {!userId && (
+                                                <Button className="w-full">
+                                                    <Link href="/sign-in">Start Now</Link>
+                                                </Button>
+                                            )}
+                                            {userId && (
+                                                <Button onClick={() => handleCheckout(price)} className="w-full">
+                                                    Checkout
+                                                </Button>
+                                            )}
+                                        </div>
                                     </div>
-                                </CardContent>
-                                <CardFooter></CardFooter>
-                            </Card>
-                        ))}
-                </div>
-            </div>
+                                ))}
+                            </div>
+                        </CardContent>
+                        <CardFooter></CardFooter>
+                    </Card>
+                ))}
         </div>
     )
 }
