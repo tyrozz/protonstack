b5a4810b41c79cd60ae0b26d4dc57023fbfe8d6c
ozan yildirim <ozanyildirim1000@gmail.com>
2023-07-31 00:51:57 +0100

    error handling for stripe api calls added

--- src/components/subscription-details.tsx
+++ src/components/subscription-details.tsx
@@ -1,23 +1,28 @@
 import { SubscriptionWithProduct } from '@/types/subscriptions'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { ManageSubscriptionButton } from '@/components/manage-subscription-button'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 interface SubscriptionDetailsProps {
     subscription: SubscriptionWithProduct
 }
 
 export function SubscriptionDetails({ subscription }: SubscriptionDetailsProps) {
-    const { status } = subscription
-
     return (
         <Card className="w-[350px]">
             <CardHeader>
                 <CardTitle>Your Subscription</CardTitle>
                 <CardDescription>You can see your subscription details and update your subscription </CardDescription>
             </CardHeader>
-            <CardContent>Status: {status}</CardContent>
+            <CardContent>
+                {subscription && subscription?.status ? `Status: ${subscription?.status}` : 'You have no subscription'}{' '}
+            </CardContent>
             <CardFooter className="flex justify-between">
-                <ManageSubscriptionButton />
+                {subscription && subscription.status && <ManageSubscriptionButton />}
+                <Link href="/pricing">
+                    <Button>Pricing</Button>
+                </Link>
             </CardFooter>
         </Card>
     )
--- src/lib/subscriptions.ts
+++ src/lib/subscriptions.ts
@@ -38,61 +38,70 @@ export const getUserSubscription = async () => {
         return null
     }
 
+    if (!user.privateMetadata) {
+        return null
+    }
+
     const userPrivateMetadata = userPrivateMetadataSchema.parse(user.privateMetadata)
 
-    const stripePrice = await stripe.prices.retrieve(userPrivateMetadata.stripePriceId as string)
-    const stripeSubscription = await stripe.subscriptions.retrieve(userPrivateMetadata.stripeSubscriptionId as string)
-    const stripeProduct = await stripe.products.retrieve(stripePrice.product as string)
+    try {
+        const stripePrice = await stripe.prices.retrieve(userPrivateMetadata.stripePriceId as string)
+        const stripeSubscription = await stripe.subscriptions.retrieve(
+            userPrivateMetadata.stripeSubscriptionId as string
+        )
+        const stripeProduct = await stripe.products.retrieve(stripePrice.product as string)
 
-    const price = {
-        id: stripePrice.id,
-        product_id: stripePrice.product,
-        active: stripePrice.active,
-        currency: stripePrice.currency,
-        type: stripePrice.type,
-        unit_amount: stripePrice.unit_amount,
-        interval: stripePrice.recurring?.interval,
-        interval_count: stripePrice.recurring?.interval_count,
-        metadata: stripePrice.metadata,
-        billing_scheme: stripePrice.billing_scheme,
-        recurring: {
-            aggregate_usage: stripePrice.recurring?.aggregate_usage,
+        const price = {
+            id: stripePrice.id,
+            product_id: stripePrice.product,
+            active: stripePrice.active,
+            currency: stripePrice.currency,
+            type: stripePrice.type,
+            unit_amount: stripePrice.unit_amount,
             interval: stripePrice.recurring?.interval,
             interval_count: stripePrice.recurring?.interval_count,
-            trial_period_days: stripePrice.recurring?.trial_period_days,
-            usage_type: stripePrice.recurring?.usage_type,
-        },
-    } as Price
+            metadata: stripePrice.metadata,
+            billing_scheme: stripePrice.billing_scheme,
+            recurring: {
+                aggregate_usage: stripePrice.recurring?.aggregate_usage,
+                interval: stripePrice.recurring?.interval,
+                interval_count: stripePrice.recurring?.interval_count,
+                trial_period_days: stripePrice.recurring?.trial_period_days,
+                usage_type: stripePrice.recurring?.usage_type,
+            },
+        } as Price
 
-    const subscription = {
-        cancel_at: stripeSubscription.cancel_at,
-        cancel_at_period_end: stripeSubscription.cancel_at_period_end,
-        canceled_at: stripeSubscription.canceled_at,
-        created: stripeSubscription.created,
-        current_period_end: stripeSubscription.current_period_end,
-        current_period_start: stripeSubscription.current_period_start,
-        ended_at: stripeSubscription.ended_at,
-        id: stripeSubscription.id,
-        metadata: stripeSubscription.metadata,
-        price_id: stripeSubscription.items.data[0].price.id,
-        quantity: stripeSubscription.items.data[0].quantity,
-        status: stripeSubscription.status,
-        trial_end: stripeSubscription.trial_end,
-        trial_start: stripeSubscription.trial_start,
-        user_id: stripeSubscription.customer,
-    } as Subscription
+        const subscription = {
+            cancel_at: stripeSubscription.cancel_at,
+            cancel_at_period_end: stripeSubscription.cancel_at_period_end,
+            canceled_at: stripeSubscription.canceled_at,
+            created: stripeSubscription.created,
+            current_period_end: stripeSubscription.current_period_end,
+            current_period_start: stripeSubscription.current_period_start,
+            ended_at: stripeSubscription.ended_at,
+            id: stripeSubscription.id,
+            metadata: stripeSubscription.metadata,
+            price_id: stripeSubscription.items.data[0].price.id,
+            quantity: stripeSubscription.items.data[0].quantity,
+            status: stripeSubscription.status,
+            trial_end: stripeSubscription.trial_end,
+            trial_start: stripeSubscription.trial_start,
+            user_id: stripeSubscription.customer,
+        } as Subscription
 
-    const product = {
-        ...stripeProduct,
-    } as Product
+        const product = {
+            ...stripeProduct,
+        } as Product
 
-    const subscriptionDetails = {
-        ...subscription,
-        prices: {
-            ...price,
-            products: product,
-        },
-    } as SubscriptionWithProduct
-
-    return subscriptionDetails
+        const subscriptionDetails = {
+            ...subscription,
+            prices: {
+                ...price,
+                products: product,
+            },
+        } as SubscriptionWithProduct
+        return subscriptionDetails
+    } catch (error) {
+        return null
+    }
 }
